10be20be7713f16ad501c5fb412c004830043ce5
0ldude <geniodelletartarughe@protonmail.com>
2022-11-23 21:38:58 +0100

    better contrast bg/fg

--- lua/highlight-groups/nvim.lua
+++ lua/highlight-groups/nvim.lua
@@ -1,9 +1,9 @@
 local hr = tonumber(os.date('%H', os.time()))
 if hr > 7 and hr < 17 then -- day between 7am and 5pm
     vim.opt.background = 'light'
-
-    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#FBF1C7" })
-    --vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#1D2021" })
+    vim.api.nvim_set_gruvbox_contrast_light = 'hard'
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#FBF1C7" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#1D2021" })
 
     vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#FBF1C7" })
     vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#FBF1C7" })
@@ -19,8 +19,9 @@ if hr > 7 and hr < 17 then -- day between 7am and 5pm
     vim.api.nvim_set_hl(0, "SignColumn", { fg = "#458588" })
 else -- night
     vim.opt.background = 'dark'
-    --vim.api.nvim_set_hl(0, "Normal", { bg = "#1D2021" })
-    --vim.api.nvim_set_hl(0, "Normal", { fg = "#FBF1C7" })
+    vim.api.nvim_set_gruvbox_contrast_dark = 'hard'
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#1D2021" })
+    vim.api.nvim_set_hl(0, "Normal", { fg = "#FBF1C7" })
 
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1D2021" })
     vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#FBF1C7" })
